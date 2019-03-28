410b2ad264194540563bd57a7e6714fc2a2bd87b
jimmywang1994 <429744781@qq.com>
2019-03-28 17:22:02 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  require('./themes/hexo-theme-snippet/gulpfile');
+});
---
+++ source/_posts/my-first-blog.md
@@ -0,0 +1,8 @@
+---
+title: my-first-blog
+categories: JAVA
+abbrlink: 9903
+date: 2019-03-28 16:25:37
+tags:
+---
+test
